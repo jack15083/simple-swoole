fefd7c2ee767cefe628dc19b55e488b65f959a4b
zengfanwei <zengfanwei@yuewen.com>
2017-05-25 09:43:35 +0800

    update

--- example/httpsrc/controller/TestController.php
+++ example/httpsrc/controller/TestController.php
@@ -5,22 +5,26 @@ use frame\log\Log;
 class TestController extends \frame\base\Controller
 {
 
-    public function actionTest()
+    public function actionHttptest()
     {
-		Log::info("action test");
+		Log::info("action http test");
         $Totaldata = $this->getRequest();
         $rsp = $this->httpTest();
 
         $this ->send(' HELLO WORLD ' . print_r($rsp, true));
     }
 
-    private function httpTest(){
-
+    private function httpTest() {
         $model = new TestModel();
         $rsp = $model->httpTest();
-        $rsp1 = $model->dbTest();
-        Log::info(__METHOD__ . " rsp == " . print_r($rsp, true));
-        return $rsp1;
+        return $rsp;
+    }
+    
+    public function actionDbtest() {
+        Log::info("action db test");
+        $model = new TestModel();
+        $data = $model->dbTest();
+        $this ->send(print_r($data, true));
     }
 
 }
