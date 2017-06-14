420a61490c67fe6f0ecb092526559b786e891541
zengfanwei <zengfanwei@kaike.la>
2017-06-14 17:18:05 +0800

    更新数据库连接池类

--- frame/base/MysqlPool.php
+++ frame/base/MysqlPool.php
@@ -3,7 +3,8 @@ namespace frame\base;
 
 use frame\log\Log;
 
-class MysqlPool {
+class MysqlPool
+{
     const MAX_CONN = 100;
     const TIME_OUT = 1800;
     const MIN_CONN = 10; //最小连接
@@ -17,12 +18,13 @@ class MysqlPool {
 
     /**
      * [init 连接池初始化 支持多个数据库连接池]
-     * @param  [type] $connkey [description]
-     * @param  [type] $max [description]
-     * @return [type]      [description]
+     * @param  $connkey 连接关键字，可以实现多个不同的数据库连接
+     * @param  $argv [description] 配置参数
      */
-    public static function init($connkey, $argv){
-        if (empty(self::$config[$connkey]['is_init'])) {
+    public static function init($connkey, $argv)
+    {
+        if (empty(self::$config[$connkey]['is_init']))
+        {
             Log::debug(__METHOD__ . " init start ");
             self::$config[$connkey]['max'] = $argv['max'];
             self::$config[$connkey]['min'] = $argv['min'];
@@ -34,9 +36,16 @@ class MysqlPool {
         
     }
 
-    public static function start($connkey, $argv){
+    /**
+     * 开启定时任务
+     * @param $connkey
+     * @param $argv
+     */
+    public static function start($connkey, $argv)
+    {
 
-        if (!self::$timer_start) {
+        if (!self::$timer_start)
+        {
             Log::debug(__METHOD__ . " schedule ");
             //定时更新过期资源
             self::schedule($connkey, $argv);
@@ -47,12 +56,13 @@ class MysqlPool {
     }
 
     /**
-     * [getResource 分配资源]
-     * @param  [type] $callback [description]
-     * @param  [type] $argv     [description]
-     * @return [type]           [description]
+     * 获取连接资源
+     * @param $connkey
+     * @param $argv
+     * @return array
      */
-    public static function getResource($connkey, $argv){
+    public static function getResource($connkey, $argv)
+    {
         if(empty($argv['max'])) $argv['max'] = self::MAX_CONN;
         if(empty($argv['min'])) $argv['min'] = self::MIN_CONN;
         if(empty($argv['timeout'])) $argv['timeout'] = self::TIME_OUT;
@@ -60,7 +70,8 @@ class MysqlPool {
         self::init($connkey, $argv);
         self::start($connkey, $argv);
 
-        if (!self::$free_queue[$connkey]->isEmpty()) {
+        if (!self::$free_queue[$connkey]->isEmpty())
+        {
             //现有资源可处于空闲状态
             $key = self::$free_queue[$connkey]->dequeue();
             Log::debug(__METHOD__ . " free queue  key == $key ", __CLASS__);
@@ -68,22 +79,27 @@ class MysqlPool {
             return array(
                 'r' => 0,
                 'key' => $key,
-                'data' => self::update($connkey, $key, $argv), //更新一些标记字段
-                );
+                'data' => self::update($connkey, $key, $argv), 
+            );
         }
 
-        elseif (count(self::$working_pool[$connkey]) < self::$config[$connkey]['max']) {
+        elseif (count(self::$working_pool[$connkey]) < self::$config[$connkey]['max'])
+        {
             Log::debug(__METHOD__ . " below max, current count:" . $key, __CLASS__);
-            if(self::$close_queue[$connkey]->isEmpty()) {
+            if(self::$close_queue[$connkey]->isEmpty())
+            {
                 $key = count(self::$working_pool[$connkey]);
             }
-            else {
+            else
+            {
                 $key = self::$close_queue[$connkey]->dequeue();
             }
+
             //当前池可以再添加资源用于分配         
             $resource = self::product($connkey, $argv);
             //product失败
-            if(!$resource) {
+            if(!$resource)
+            {
                 Log::info('product resource error:' . $connkey . $key);
                 return array('r' => 1);
             }
@@ -94,9 +110,10 @@ class MysqlPool {
                 'r' => 0,
                 'key' => $key,
                 'data' => self::$working_pool[$connkey][$key]['obj'],
-                );
+            );
         }
-        else{
+        else
+        {
             Log::error(__METHOD__ . " no resource can apply ", __CLASS__);           
             return array('r' => 1);
         }
@@ -105,10 +122,11 @@ class MysqlPool {
 
     /**
      * [freeResource 释放资源]
-     * @param  [type] $argv [description]
-     * @return [type]       [description]
+     * @param $connkey
+     * @param $key
      */
-    public static function freeResource($connkey, $key){
+    public static function freeResource($connkey, $key)
+    {
         Log::debug(__METHOD__ . " key == $key", __CLASS__);
         self::$free_queue[$connkey]->enqueue($key);
         self::$working_pool[$connkey][$key]['status'] = 0;
@@ -116,17 +134,22 @@ class MysqlPool {
 
     /**
      * [schedule 定时调度 更新过期资源]
-     * @param timeout seconds
-     * @return [type] [description]
+     * @param $connkey
+     * @param $argv
      */
-    public static function schedule($connkey, $argv){
+    public static function schedule($connkey, $argv)
+    {
         Log::debug(__METHOD__ . 'schedule start:' . $argv['timeout'] . 's');
-        swoole_timer_tick($argv['timeout'] * 1000, function() use($argv) {          
+        swoole_timer_tick($argv['timeout'] * 1000, function() use($argv)
+        {
             Log::debug('schedule timer tick start');
-            foreach (self::$working_pool as $connkey => $pool_data) {
-                foreach ($pool_data as $key => $data) {
+            foreach (self::$working_pool as $connkey => $pool_data)
+            {
+                foreach ($pool_data as $key => $data)
+                {
                     //当前连接已超时
-                    if($data['status'] != 0 && $data['lifetime'] < microtime(true)) {
+                    if($data['status'] != 0 && $data['lifetime'] < microtime(true))
+                    {
                         //释放资源
                         self::freeResource($connkey, $key);
                     }                    
@@ -136,19 +159,23 @@ class MysqlPool {
     }
     
     /**
-     * 定时回收超时连接资源
+     * 定时回收多余空闲连接资源
      * @param string $connkey
      * @param array $argv
      */
-    public static function recovery($connkey, $argv){
+    public static function recovery($connkey, $argv)
+    {
         Log::debug(__METHOD__ . 'recovery start:' . self::RECOVERY_TIME_INTERVAL);
-        swoole_timer_tick(self::RECOVERY_TIME_INTERVAL, function() use($argv) {
+        swoole_timer_tick(self::RECOVERY_TIME_INTERVAL, function() use($argv)
+        {
             Log::debug('recovery timer tick start');
-            foreach (self::$free_queue as $connkey => $queue) {
+            foreach (self::$free_queue as $connkey => $queue)
+            {
                 if($queue->isEmpty() || $queue->count() <= $argv['min']) 
                     continue;               
                 //空闲资源超过最小连接，关闭多余的数据库连接
-                for($i = $argv['min']; $i < $queue->count();) {
+                for($i = $argv['min']; $i < $queue->count();)
+                {
                     $key = $queue->dequeue();
                     //关闭数据库连接
                     self::$working_pool[$connkey][$key]['obj']->close();
@@ -159,11 +186,14 @@ class MysqlPool {
             }
         });
     }
+
     /**
      * [product 生产资源]
-     * @return [type] [description]
+     * @param $connkey
+     * @param $argv
      */
-    private static function product($connkey, $argv){
+    private static function product($connkey, $argv)
+    {
         //防止并发出现已超过连接数
         if(count(self::$working_pool[$connkey]) >= self::$config[$connkey]['max']) 
             return false;
@@ -174,17 +204,17 @@ class MysqlPool {
             'obj' => $resource,                                             //实例
             'lifetime' => microtime(true) + floatval($argv['timeout']),   //生命期
             'status' => 1,                                                //状态 1 在用 0 空闲
-            );
+        );
     }
 
     /**
      * [update 更新资源]
-     * @param  [type] $key  [description]
-     * @param  [type] $argv [description]
-     * @return [type]       [description]
+     * @param string $connkey
+     * @param string $key
+     * @param array $argv
      */
-    private static function update($connkey, $key, $argv){
-
+    private static function update($connkey, $key, $argv)
+    {
         self::$working_pool[$connkey][$key]['status'] = 1;
         self::$working_pool[$connkey][$key]['lifetime'] = microtime(true) + floatval($argv['timeout']);
         return self::$working_pool[$connkey][$key]['obj'];
@@ -192,9 +222,9 @@ class MysqlPool {
     
     /**
      * 更新数据库连接
-     * @param unknown $connkey
-     * @param unknown $key
-     * @param unknown $obj
+     * @param string $connkey
+     * @param string $key
+     * @param array $argv
      */
     public static function updateConnect($connkey, $key, $argv)
     {
