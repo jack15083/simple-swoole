0b2d568b66c7be54f85b77fe3f458ec3a47a3fb5
zengfanwei <zengfanwei@kaike.la>
2017-11-13 11:13:12 +0800

    update router

--- example/httpsrc/route/web.php
+++ example/httpsrc/route/web.php
@@ -3,3 +3,4 @@
 $router->get('/test', 'TestController@actionTest');
 $router->get('/test1', 'TestController@actionTest');
 $router->get('/test.html', 'TestController@actionTest');
+$router->get('/test/{username}/{id2}/{id3}', 'TestController@actionTest');
--- frame/base/RouteRegex.php
+++ frame/base/RouteRegex.php
@@ -6,31 +6,77 @@ use frame\log\Log;
 
 class RouteRegex extends Protocol
 {
-
+    /**
+     * Match the request uri
+     * @param $req
+     * @return bool|Route
+     */
     public function onRoute($req) {
 
-        $uri    = strtolower($req->data['server']['request_uri']);
+        $uri    = $req->data['server']['request_uri'];
         $method = $req->data['server']['request_method'];
         $path   = '/' . trim($uri, '/');
 
-        Log::debug('--------start debug route---------');
-        Log::debug($uri);
-        Log::debug($method);
-        Log::debug($path);
-
         $routes = $this->router->getRoutes();
+        if (isset($routes[$method . $path])) {
+            $route  = $routes[$method . $path];
+        } else {
+            $route = $this->getRouteRegx($method, $path, $routes);
+        }
 
-        Log::debug(print_r($routes, true));
-
-        if(!isset($routes[$method . $path]))
-            return false;
-
-        $route  = $routes[$method . $path];
+        if(empty($route)) return false;
 
         $action = $route['action'];
         $arr = explode('@', $action);
+        return new Route($arr[0], $arr[1], $route['get']);
+    }
 
-        return new Route($arr[0], $arr[1], []);
+    /**
+     * get route by regex
+     * @param $method
+     * @param $path
+     * @param $routes
+     * @return mixed
+     */
+    protected function getRouteRegx($method, $path, $routes) {
+        $pathArr = explode('/', $path);
+
+        foreach ($routes as $uri => $row) {
+            preg_match_all('/\{(\w+)\}/', $uri, $matches);
+
+            if (empty($matches[1]))
+                continue;
+
+            $matchCount = count($matches[1]);
+
+            if (count($pathArr) < $matchCount)
+                continue;
+
+            for ($i = 0 ; $i < $matchCount; $i++) {
+                $getVal[$matchCount - $i] = array_pop($pathArr);
+            }
+
+            $pathReg = '#^' . $method . implode('/', $pathArr);
+            foreach ($matches[1] as $key => $name) {
+                $pathReg .= '/\{(\w+)\}';
+            }
+            $pathReg .= '$#';
+
+            if(!preg_match($pathReg, $uri, $matchesKey)) {
+                return false;
+            }
+
+            foreach ($matchesKey as $key => $item) {
+                if ($key == 0)  continue;
+                $get[$item] = isset($getVal[$key]) ? $getVal[$key] : '';
+            }
+
+            $row['get'] = $get;
+
+            return $row;
+        }
+
+        return false;
     }
 
 }
